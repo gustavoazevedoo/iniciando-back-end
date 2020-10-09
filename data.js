cd4e7e11653780a33632c3c30e953df461fdc8b7
Gustavo Azevedo <gustavosazevedo@gmail.com>
2020-10-08 23:17:37 -0300

    finalizando-modulo

--- data.js
+++ data.js
@@ -3,7 +3,8 @@ module.exports = [
         id: "GykTLqODQuU",
         title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
         duration: "57 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     },
     {
         id: "HN1UjzRSdBk",
@@ -15,7 +16,8 @@ module.exports = [
         id: "rAzHvYnQ8DY",
         title: "Array: Higher Order Functions | Mayk Brito",
         duration: "54 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     },
     {
         id: "ghTrp1x_1As",
@@ -27,7 +29,8 @@ module.exports = [
         id: "GSqR2i-Pq6o",
         title: "Desvendando a variável this no Javascript | Mayk Brito",
         duration: "48 min",
-        price: "Free"
+        price: "Free",
+        featured: true
     },
     {
         id: "2alg7MQ6_sI",
